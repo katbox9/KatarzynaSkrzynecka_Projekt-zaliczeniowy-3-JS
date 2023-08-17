834b0b59bdaf60d5ade2173073303ee65e420046
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-08-17 19:40:51 +0200

    add output element

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const form = document.getElementById("currency-container");
+const input = document.getElementById("amount-to-be-calculated");
+const select = document.getElementById("currency");
+const btn = document.getElementById("btn");
 
-function convertCurrency() {
+function convertCurrency(event) {
+  event.preventDefault();
   axios
-    .get("https://api.nbp.pl/?ref=public-apis")
-
-    .then((response) => console.log(response.data))
+    .get(
+      `https://api.nbp.pl/api/exchangerates/rates/a/${select.value}/?format=json`
+    )
+    .then((response) => calculateResult(response.data.rates[0].mid))
     .catch((error) => console.log(error));
 }
 
-// const convertCurrency() => {}
+// function calculateResult(rate) {
+//   return input * rate;
+// }
 
 form.addEventListener("submit", convertCurrency);
