0eadde197c6f50096f9b0df860ea628ea3bd869f
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-08-20 12:59:15 +0200

    add style to input and select

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const input = document.getElementById("amount-to-be-calculated");
 const select = document.getElementById("currency");
 const btn = document.getElementById("btn");
 
-function convertCurrency(event) {
+function getExchangeRate(event) {
   event.preventDefault();
   axios
     .get(
       `https://api.nbp.pl/api/exchangerates/rates/a/${select.value}/?format=json`
     )
-    .then((response) => calculateResult(response.data.rates[0].mid))
+    .then((response) => getExchangeRate(response.data.rates[0].mid))
     .catch((error) => console.log(error));
 }
 
-// function calculateResult(rate) {
-//   return input * rate;
-// }
+function convertCurrency(rate) {
+  const rate = response?.data?.rates[0]?.mid;
+  if (rate) {
+    const userInput = input.value;
+    return userInput * rate;
+  } else {
+    alert("Mamy problem. Spróbuj ponownie za kilka minut.");
+  }
+}
 
 form.addEventListener("submit", convertCurrency);
